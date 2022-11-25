b94d69c30e1cdf2058aa0b4ac0ffaa9b96f88765
Andre Ferreira <AFerreira@PFWork.com.au>
2022-11-25 20:04:15 +1100

    Random Changes

---
+++ Model/Folders/Folder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SafetyCulture.Model.Folders
+{
+    public class Folder
+    {
+        /// <summary>
+        /// Unique ID
+        /// </summary>
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Name of the Site
+        /// </summary>
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = default!;
+        /// <summary>
+        /// Organisational ID
+        /// </summary>
+        [JsonPropertyName("org_id")]
+        public Guid OrgId { get; set; }
+        /// <summary>
+        /// Creator ID
+        /// </summary>
+        [JsonPropertyName("creator_id")]
+        public Guid CreatorId { get; set; }
+        /// <summary>
+        /// Created on this Date
+        /// </summary>
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Modified on this date
+        /// </summary>
+        [JsonPropertyName("modified_at")]
+        public DateTime ModifiedAt { get; set; }
+        /// <summary>
+        /// Location/Region/Area
+        /// </summary>
+        [JsonPropertyName("meta_label")]
+        public string MetaLabel { get; set; } = default!;
+
+    }
+}
--- Model/Folders/FolderResponse.cs
+++ Model/Folders/FolderResponse.cs
@@ -7,44 +7,10 @@ using System.Threading.Tasks;
 
 namespace SafetyCulture.Model.Folders
 {
+
     public class FolderResponse
     {
-        /// <summary>
-        /// Unique ID
-        /// </summary>
-        [JsonPropertyName("id")]
-        public Guid Id { get; set; }
-        /// <summary>
-        /// Name of the Site
-        /// </summary>
-        [JsonPropertyName("name")]
-        public string Name { get; set; } = default!;
-        /// <summary>
-        /// Organisational ID
-        /// </summary>
-        [JsonPropertyName("org_id")]
-        public Guid OrgId { get; set; }
-        /// <summary>
-        /// Creator ID
-        /// </summary>
-        [JsonPropertyName("creator_id")]
-        public Guid CreatorId { get; set; }
-        /// <summary>
-        /// Created on this Date
-        /// </summary>
-        [JsonPropertyName("created_at")]
-        public DateTime CreatedAt { get; set; }
-
-        /// <summary>
-        /// Modified on this date
-        /// </summary>
-        [JsonPropertyName("modified_at")]
-        public DateTime ModifiedAt { get; set; }
-        /// <summary>
-        /// Location/Region/Area
-        /// </summary>
-        [JsonPropertyName("meta_label")]
-        public string MetaLabel { get; set; } = default!;
-
+        [JsonPropertyName("folder")]
+        public Folder Folder { get; set; } = default!;
     }
 }
